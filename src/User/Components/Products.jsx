fdcd3b2202b89b4662542162a2c1aea732109dff
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-06-08 10:29:32 +0530

    Front end project

---
+++ src/User/Components/Products.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function Products() {
+    const [items, setitems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/products")
+            .then(res => res.json())
+            .then(data => setitems(data))
+
+    }, [])
+
+    return (
+        
+        <>
+            <h1 className='text-5xl text-black font-bold '>Products</h1>
+            <div className='flex justify-evenly flex-wrap'>
+                {items.map((item) => (
+                    <div key={item.id} className="card w-80 bg-white shadow-xl text-black flex m-3">
+                        <figure className="px-10 pt-10">
+                            <img src={item.URL} alt="petfood" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h4>{item.Category} Food</h4>
+                            <h2 className="card-title">{item.productname}</h2>
+                            <div className="text-xl font-bold">₹{item.Price}</div>
+                            <div className="card-actions">
+                                <Link to={`/detailspage/${item.id}`}><button className="btn btn-primary">Show more</button></Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Products
